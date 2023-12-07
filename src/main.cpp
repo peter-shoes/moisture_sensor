fbe36095f982b0df066ce176316bc07a0955091b
Peter Shoes <bruce.mcculloch64@gmail.com>
2023-12-06 21:44:39 -0500

    final

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 #include <Adafruit_IS31FL3731.h>
 
 #define DEBUG       true
-#define DEBUG_SLEEP 100             // one second, how often we read in debug mode
+#define DEBUG_SLEEP 100             // 0.1 seconds, how often we read in debug mode
 #define OP_SLEEP    3600000         // one hour, how often we read during normal operation
-#define READ_SLEEP  10000           // ten seconds, time between reading during read step 
+#define READ_SLEEP  100             // 0.1 seconds, time between reading during read step 
 #define S_READ_BUF  10              // size of our read buffer per sensor (preprocessor OK)
 #define SCREEN_HI   100
 #define SCREEN_LO   20 
 
-#define V_AIR       570             // sensor measurement for air (ground truth)
+#define V_AIR       590             // sensor measurement for air (ground truth)
 #define V_DRY_SOIL  520             // sensor measurement for dry soil (ground truth)
 #define V_WET_SOIL  356             // sensor measurement for wet soil (ground truth)
 #define V_WATER     321             // sensor measurement for pure water or very wet soil (ground truth)
@@ -21,8 +21,8 @@ Adafruit_IS31FL3731_Wing ledmatrix = Adafruit_IS31FL3731_Wing();
 int a0_buf[S_READ_BUF];
 int a1_buf[S_READ_BUF];
 int a2_buf[S_READ_BUF];
-int normalized_reading;
-int moisture_value;
+int normalized_reading = 0;
+int moisture_value = 0;
 
 // define functions
 void print_arr(int a[S_READ_BUF], String name);
@@ -50,22 +50,21 @@ void loop() {
   // Serial.println("Waking from sleep ...");
   
   // get measurements (Sensor Data Extraction)
-  // read_data();
+  read_data();
   
   // if we're debugging, print those measurements
-  // print_arr(a0_buf, "A0: ");
-  // print_arr(a1_buf, "A1: ");
-  // print_arr(a2_buf, "A2: ");
+  print_arr(a0_buf, "A0: ");
+  print_arr(a1_buf, "A1: ");
+  print_arr(a2_buf, "A2: ");
   
   // normalize measurements (Preprocessing)
-  // normalize_data();
+  normalize_data();
   
   // convert measurements to percentage moisture (Feature Extraction / Classification)
-  // reading_to_value();
+  reading_to_value();
   
   // update display (Meaningful Output)
-  // update_display(moisture_value);
-  update_display(60);
+  update_display(moisture_value);
 }
 
 
@@ -96,9 +95,9 @@ void normalize_data()
 {
   // Here, we want to take the raw data and turn it into a single useful value
   // begin by computing averages for each buffer
-  int a0_avg;
-  int a1_avg;
-  int a2_avg;
+  int a0_avg = 0;
+  int a1_avg = 0;
+  int a2_avg = 0;
   for (int i=0; i < S_READ_BUF; i++)
   {
     a0_avg += a0_buf[i];
@@ -113,9 +112,6 @@ void normalize_data()
   // so we want to weigh each by its standard deviation
   // for now this is okay
   normalized_reading = (a0_avg+a1_avg+a2_avg)/3;
-  // but we want to bound this value explicitly, so let's take care of that
-  if (normalized_reading > 100) {normalized_reading = 100;}
-  if (normalized_reading < 0) {normalized_reading = 0;}
 }
 
 
@@ -124,7 +120,11 @@ void reading_to_value()
   // Now we must convert the reading we have gotten into a usable moisture value
   // we know the sensors are bounded by our ground truths for air and water
   // so let's convert this to a value between 0 and 100 based on those bounds
-  moisture_value = ((normalized_reading - V_WATER)/(V_AIR-V_WATER)) * 100;
+  moisture_value = (float(V_AIR - normalized_reading)/float(V_AIR-V_WATER))*100;
+
+  // but we want to bound this value explicitly, so let's take care of that
+  if (moisture_value > 100) {moisture_value = 100;}
+  if (moisture_value < 0) {moisture_value = 0;}
 }
 
 
@@ -138,6 +138,11 @@ void update_display(int val)
   ledmatrix.drawFastHLine(0,0,15, SCREEN_LO);
   ledmatrix.drawFastHLine(0,6,15, SCREEN_LO);
 
+  // then we need to clear the bars so that they can be written over
+  for (int i=1; i<14; i++)
+  {
+    ledmatrix.drawFastVLine(i, 1, 5, 0);
+  }
   // now we can actually do some conversions
   // we have a value from 0-100, and we want to convert that into a value from 1-14, since we have 14 available bars
   for (int i=1; i<(val*0.14); i++)
